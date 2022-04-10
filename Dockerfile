eaf5a3ad0b249ea16ecb3897a5e45f97a7873b7f
Simon Szustkowski <mail@simonszu.de>
2022-04-10 22:04:42 +0200

    Added some code for reading serial stuff

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,9 @@ ENV PYTHONDONTWRITEBYTECODE=1
 # Turns off buffering for easier container logging
 ENV PYTHONUNBUFFERED=1
 
+# Specifies the device where the serial connector to the NFM device is located
+ENV SERIAL=/dev/ttyUSB0
+
 # Install pip requirements
 COPY requirements.txt .
 RUN python -m pip install -r requirements.txt
--- nfm.py
+++ nfm.py
@@ -1,7 +1,7 @@
-import time
+#! /usr/bin/python3
+import time, serial, io
 from prometheus_client.core import GaugeMetricFamily, REGISTRY, CounterMetricFamily
 from prometheus_client import start_http_server
-
 
 class CustomCollector(object):
     def __init__(self):
@@ -18,7 +18,31 @@ class CustomCollector(object):
 
 
 if __name__ == '__main__':
-    start_http_server(8000)
-    REGISTRY.register(CustomCollector())
-    while True:
-        time.sleep(1)
+#    start_http_server(8000)
+#    REGISTRY.register(CustomCollector())
+#    while True:
+#        time.sleep(1)
+
+  ser = serial.Serial( 
+    port = '/dev/ttyUSB0',
+    baudrate = 19200,
+    bytesize = serial.EIGHTBITS,
+    parity = serial.PARITY_NONE,
+    stopbits = serial.STOPBITS_ONE
+  )
+  try:
+    ser.open()
+  except IOError: # if port is already opened, close and open it again
+    ser.close()
+    ser.open()
+    print ("port was already open, was closed and opened again!")
+  except Exception as e:
+    print("error open serial port: " + str(e))
+    exit()
+
+  while True:
+    line=ser.readline().decode("utf-8")[:-2]
+    print(line)
+
+  print('Done')
+  ser.close()
