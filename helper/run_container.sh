265e5df7970674efd41cca31d8280ea3737bd62a
Simon Szustkowski <mail@simonszu.de>
2022-04-14 18:16:09 +0200

    Run container and app privileged

--- helper/run_container.sh
+++ helper/run_container.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-docker run -d --publish 8000:8000 --device /dev/ttyUSB0 -e SERIAL='/dev/ttyUSB0' --name nfm nfm
+docker run -d --publish 8000:8000 --privileged --device /dev/ttyUSB0 -e SERIAL='/dev/ttyUSB0' --name nfm nfm
